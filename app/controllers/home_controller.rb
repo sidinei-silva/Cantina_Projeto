1353e43a61e9f08f12639e810486c605222a9d8c
Sidinei Silva <sidinei.silva02@gmail.com>
2018-08-02 11:40:50 -0300

    Mudança ordem javascript

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
   def details
     begin
       @produto = Produto.find(params[:format])
+      @adicional = Adicional.where(produto_id: @produto.id)
     rescue ActiveRecord::RecordNotFound => e
       flash[:notice] = "Produto não encontrado"
       redirect_to root_url
